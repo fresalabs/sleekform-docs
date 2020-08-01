605c3a7c18af8698e3b864049cf9be05aff2c0ff
Mohith G <mohithgm@gmail.com>
2020-08-01 22:16:23 +0530

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Sleek Form', // Title for your website.
   tagline: 'Typeform components built with React',
-  url: 'https://sleek-form.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://fresalabs.github.io', // Your website URL
+  baseUrl: '/sleekform/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'sleek-form-docs',
+  projectName: 'sleekform',
   organizationName: 'fresalabs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
